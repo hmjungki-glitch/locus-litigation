a6b2f7cd78930cd130f7d4bd4d67b55efe07e335
hmjungki-glitch <hm.jungki@gmail.com>
2026-03-25 20:11:11 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
   "https://piianthaxklcpqoyylkv.supabase.co",
-  "여기에_전체_anon_key"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpaWFudGhheGtsY3Bxb3l5bGt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0Mjg0NDUsImV4cCI6MjA5MDAwNDQ0NX0.uYGQ3UTQJ3eyFaMay8UhXPHM6enXjJhukGiVlJEfwrQ"
 );
 
 export default function Home() {
